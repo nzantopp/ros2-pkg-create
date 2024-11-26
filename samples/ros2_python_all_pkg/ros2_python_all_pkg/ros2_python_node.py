bddfdf08d0edab1015aef84c059d5e8331e2e044
Nico Zantopp <nico.zantopp@cml.fraunhofer.de>
2024-11-26 22:53:26 +0100

    Fixed all ament_flake8 problems

--- samples/ros2_python_all_pkg/ros2_python_all_pkg/ros2_python_node.py
+++ samples/ros2_python_all_pkg/ros2_python_all_pkg/ros2_python_node.py
@@ -1,21 +1,25 @@
 import time
 from typing import Any, Optional, Union
 
+from rcl_interfaces.msg import (
+    FloatingPointRange,
+    IntegerRange,
+    ParameterDescriptor,
+    SetParametersResult
+)
 import rclpy
 from rclpy.action import ActionServer, CancelResponse, GoalResponse
 from rclpy.node import Node
-from rcl_interfaces.msg import (FloatingPointRange, IntegerRange, ParameterDescriptor, SetParametersResult)
+from ros2_python_all_pkg_interfaces.action import Fibonacci
 from std_msgs.msg import Int32
 from std_srvs.srv import SetBool
-from ros2_python_all_pkg_interfaces.action import Fibonacci
 
 
 class Ros2PythonNode(Node):
 
     def __init__(self):
-        """Constructor"""
-
-        super().__init__("ros2_python_node")
+        """Initialize the node."""
+        super().__init__('ros2_python_node')
 
         self.auto_reconfigurable_params = []
         self.parameters_callback = None
@@ -23,9 +27,9 @@ class Ros2PythonNode(Node):
         self.publisher = None
 
         self.param = 1.0
-        self.param = self.declareAndLoadParameter(name="param",
+        self.param = self.declareAndLoadParameter(name='param',
                                                   param_type=rclpy.Parameter.Type.DOUBLE,
-                                                  description="TODO",
+                                                  description='TODO',
                                                   default=self.param,
                                                   from_value=0.0,
                                                   to_value=10.0,
@@ -33,19 +37,20 @@ class Ros2PythonNode(Node):
 
         self.setup()
 
-    def declareAndLoadParameter(self,
-        name: str,
-        param_type: rclpy.Parameter.Type,
-        description: str,
-        default: Optional[Any] = None,
-        add_to_auto_reconfigurable_params: bool = True,
-        is_required: bool = False,
-        read_only: bool = False,
-        from_value: Optional[Union[int, float]] = None,
-        to_value: Optional[Union[int, float]] = None,
-        step_value: Optional[Union[int, float]] = None,
-        additional_constraints: str = "") -> Any:
-        """Declares and loads a ROS parameter
+    def declareAndLoadParameter(
+            self,
+            name: str,
+            param_type: rclpy.Parameter.Type,
+            description: str,
+            default: Optional[Any] = None,
+            add_to_auto_reconfigurable_params: bool = True,
+            is_required: bool = False,
+            read_only: bool = False,
+            from_value: Optional[Union[int, float]] = None,
+            to_value: Optional[Union[int, float]] = None,
+            step_value: Optional[Union[int, float]] = None,
+            additional_constraints: str = '') -> Any:
+        """Declare and load a ROS parameter.
 
         Args:
             name (str): name
@@ -63,7 +68,6 @@ class Ros2PythonNode(Node):
         Returns:
             Any: parameter value
         """
-
         # declare parameter
         param_desc = ParameterDescriptor()
         param_desc.description = description
@@ -72,12 +76,22 @@ class Ros2PythonNode(Node):
         if from_value is not None and to_value is not None:
             if param_type == rclpy.Parameter.Type.INTEGER:
                 step_value = step_value if step_value is not None else 1
-                param_desc.integer_range = [IntegerRange(from_value=from_value, to_value=to_value, step=step_value)]
+                param_desc.integer_range = [
+                    IntegerRange(
+                        from_value=from_value, to_value=to_value, step=step_value
+                    )
+                ]
             elif param_type == rclpy.Parameter.Type.DOUBLE:
                 step_value = step_value if step_value is not None else 1.0
-                param_desc.floating_point_range = [FloatingPointRange(from_value=from_value, to_value=to_value, step=step_value)]
+                param_desc.floating_point_range = [
+                    FloatingPointRange(
+                        from_value=from_value, to_value=to_value, step=step_value
+                    )
+                ]
             else:
-                self.get_logger().warn(f"Parameter type of parameter '{name}' does not support specifying a range")
+                self.get_logger().warn(
+                    f"Parameter type of parameter '{name}' does not support specifying a range"
+                )
         self.declare_parameter(name, param_type, param_desc)
 
         # load parameter
@@ -86,10 +100,14 @@ class Ros2PythonNode(Node):
             self.get_logger().info(f"Loaded parameter '{name}': {param}")
         except rclpy.exceptions.ParameterUninitializedException:
             if is_required:
-                self.get_logger().fatal(f"Missing required parameter '{name}', exiting")
+                self.get_logger().fatal(
+                    f"Missing required parameter '{name}', exiting"
+                )
                 raise SystemExit(1)
             else:
-                self.get_logger().warn(f"Missing parameter '{name}', using default value: {default}")
+                self.get_logger().warn(
+                    f"Missing parameter '{name}', using default value: {default}"
+                )
                 param = default
                 self.set_parameters([rclpy.Parameter(name=name, value=param)])
 
@@ -101,7 +119,7 @@ class Ros2PythonNode(Node):
 
     def parametersCallback(self,
                            parameters: rclpy.Parameter) -> SetParametersResult:
-        """Handles reconfiguration when a parameter value is changed
+        """Handle reconfiguration when a parameter value is changed.
 
         Args:
             parameters (rclpy.Parameter): parameters
@@ -109,11 +127,12 @@ class Ros2PythonNode(Node):
         Returns:
             SetParametersResult: parameter change result
         """
-
         for param in parameters:
             if param.name in self.auto_reconfigurable_params:
                 setattr(self, param.name, param.value)
-                self.get_logger().info(f"Reconfigured parameter '{param.name}' to: {param.value}")
+                self.get_logger().info(
+                    f"Reconfigured parameter '{param.name}' to: {param.value}"
+                )
 
         result = SetParametersResult()
         result.successful = True
@@ -121,33 +140,32 @@ class Ros2PythonNode(Node):
         return result
 
     def setup(self):
-        """Sets up subscribers, publishers, etc. to configure the node"""
-
+        """Set up subscribers, publishers, etc. to configure the node."""
         # callback for dynamic parameter configuration
         self.parameters_callback = self.add_on_set_parameters_callback(
             self.parametersCallback)
 
         # subscriber for handling incoming messages
         self.subscriber = self.create_subscription(Int32,
-                                                   "~/input",
+                                                   '~/input',
                                                    self.topicCallback,
                                                    qos_profile=10)
         self.get_logger().info(f"Subscribed to '{self.subscriber.topic_name}'")
 
         # publisher for publishing outgoing messages
         self.publisher = self.create_publisher(Int32,
-                                               "~/output",
+                                               '~/output',
                                                qos_profile=10)
         self.get_logger().info(f"Publishing to '{self.publisher.topic_name}'")
 
         # service server for handling service calls
-        self.service_server = self.create_service(SetBool, "~/service", self.serviceCallback)
+        self.service_server = self.create_service(SetBool, '~/service', self.serviceCallback)
 
         # action server for handling action goal requests
         self.action_server = ActionServer(
             self,
             Fibonacci,
-            "~/action",
+            '~/action',
             execute_callback=self.actionExecute,
             goal_callback=self.actionHandleGoal,
             cancel_callback=self.actionHandleCancel,
@@ -158,16 +176,19 @@ class Ros2PythonNode(Node):
         self.timer = self.create_timer(1.0, self.timerCallback)
 
     def topicCallback(self, msg: Int32):
-        """Processes messages received by a subscriber
+        """Process messages received by a subscriber.
 
         Args:
             msg (Int32): message
         """
-
         self.get_logger().info(f"Message received: '{msg.data}'")
 
-    def serviceCallback(self, request: SetBool.Request, response: SetBool.Response) -> SetBool.Response:
-        """Processes service requests
+    def serviceCallback(
+        self,
+        request: SetBool.Request,
+        response: SetBool.Response
+    ) -> SetBool.Response:
+        """Process service requests.
 
         Args:
             request (SetBool.Request): service request
@@ -176,14 +197,13 @@ class Ros2PythonNode(Node):
         Returns:
             SetBool.Response: service response
         """
-
-        self.get_logger().info("Received service request")
+        self.get_logger().info('Received service request')
         response.success = True
 
         return response
 
     def actionHandleGoal(self, goal: Fibonacci.Goal) -> GoalResponse:
-        """Processes action goal requests
+        """Process action goal requests.
 
         Args:
             goal (Fibonacci.Goal): action goal
@@ -191,13 +211,12 @@ class Ros2PythonNode(Node):
         Returns:
             GoalResponse: goal response
         """
-
-        self.get_logger().info("Received action goal request")
+        self.get_logger().info('Received action goal request')
 
         return GoalResponse.ACCEPT
 
     def actionHandleCancel(self, goal: Fibonacci.Goal) -> CancelResponse:
-        """Processes action cancel requests
+        """Process action cancel requests.
 
         Args:
             goal (Fibonacci.Goal): action goal
@@ -205,23 +224,21 @@ class Ros2PythonNode(Node):
         Returns:
             CancelResponse: cancel response
         """
-
-        self.get_logger().info("Received request to cancel action goal")
+        self.get_logger().info('Received request to cancel action goal')
 
         return CancelResponse.ACCEPT
 
     def actionHandleAccepted(self, goal: Fibonacci.Goal):
-        """Processes accepted action goal requests
+        """Process accepted action goal requests.
 
         Args:
             goal (Fibonacci.Goal): action goal
         """
-
         # execute action in a separate thread to avoid blocking
         goal.execute()
 
     async def actionExecute(self, goal: Fibonacci.Goal) -> Fibonacci.Result:
-        """Executes an action
+        """Execute an action.
 
         Args:
             goal (Fibonacci.Goal): action goal
@@ -229,8 +246,7 @@ class Ros2PythonNode(Node):
         Returns:
             Fibonacci.Result: action goal result
         """
-
-        self.get_logger().info("Executing action goal")
+        self.get_logger().info('Executing action goal')
 
         # define a sleeping rate between computing individual Fibonacci numbers
         loop_rate = 1
@@ -249,15 +265,17 @@ class Ros2PythonNode(Node):
             if goal.is_cancel_requested:
                 result.sequence = feedback.partial_sequence
                 goal.canceled()
-                self.get_logger().info("Action goal canceled")
+                self.get_logger().info('Action goal canceled')
                 return result
 
             # compute the next Fibonacci number
-            feedback.partial_sequence.append(feedback.partial_sequence[i] + feedback.partial_sequence[i-1])
+            feedback.partial_sequence.append(
+                feedback.partial_sequence[i] + feedback.partial_sequence[i-1]
+            )
 
             # publish the current sequence as action feedback
             goal.publish_feedback(feedback)
-            self.get_logger().info("Publishing action feedback")
+            self.get_logger().info('Publishing action feedback')
 
             # sleep before computing the next Fibonacci number
             time.sleep(loop_rate)
@@ -266,15 +284,13 @@ class Ros2PythonNode(Node):
         if rclpy.ok():
             result.sequence = feedback.partial_sequence
             goal.succeed()
-            self.get_logger().info("Goal succeeded")
+            self.get_logger().info('Goal succeeded')
 
         return result
 
     def timerCallback(self):
-        """Processes timer triggers
-        """
-
-        self.get_logger().info("Timer triggered")
+        """Process timer triggers."""
+        self.get_logger().info('Timer triggered')
 
 
 def main():
